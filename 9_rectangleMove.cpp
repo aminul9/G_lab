c1dac1ddab3dad0810a383a4f1bb2627560cebc7
aminul9 <aminultpi34@gmail.com>
2021-02-06 23:53:17 +0600

    Create 9_rectangleMove.cpp

---
+++ 9_rectangleMove.cpp
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+#include<string.h>
+int main()
+{
+
+	//THis code author is  from Biplob 154037
+  //THis code author is  from Biplob 154037
+  //THis code author is  from Biplob 154037, I am just collect It
+	initwindow(500,500);
+	int s,x1,y1,x2,y2,xmax,xmin,ymax,ymin,i;
+	settextstyle(SOLID_FILL,HORIZ_DIR,1);
+    outtextxy(220,10,"MD.BILOB ISLAM");
+    outtextxy(220,35,"Student ID: 154037");
+    outtextxy(220,60,"Rotate Rectangle\n");
+    outtextxy(220,90,"Bottom Rotate=2\n");
+    outtextxy(220,120,"Left Rotate=4\n");
+    outtextxy(220,150,"Right Rotate=6\n");
+    outtextxy(220,180,"Top Rotate=8\n");
+	int ty1=20;
+	xmin=200;ymin=200;
+	xmax=300;ymax=300;
+	setcolor(WHITE);
+	rectangle(xmin,ymin,xmax,ymax);
+	x1=xmin;x2=xmax;
+    y1=ymin;y2=ymax;
+	while(1)
+	{
+        char c=getch();
+        switch(c)
+        {
+            case '8':
+            {
+                setcolor(BLACK);
+                rectangle(x1,y1,x2,y2);
+                x1=x1;x2=x2;
+                y1=y1-ty1;y2=y2-ty1;
+                setcolor(WHITE);
+                rectangle(x1,y1,x2,y2);
+
+                break;
+            }
+            case '6':
+            {
+                setcolor(BLACK);
+                rectangle(x1,y1,x2,y2);
+                x1=x1+ty1;x2=x2+ty1;
+                y1=y1;y2=y2;
+                setcolor(BLACK);
+                rectangle(xmin,ymin,xmax,ymax);
+                setcolor(WHITE);
+                rectangle(x1,y1,x2,y2);
+                break;
+            }
+            case '2':
+            {   setcolor(BLACK);
+                rectangle(x1,y1,x2,y2);
+                x1=x1;x2=x2;
+                y1=y1+ty1;y2=y2+ty1;
+                setcolor(WHITE);
+                rectangle(x1,y1,x2,y2);
+
+                break;
+            }
+            case '4':
+            {   setcolor(BLACK);
+                rectangle(x1,y1,x2,y2);
+                x1=x1-ty1;x2=x2-ty1;
+                y1=y1;y2=y2;
+                setcolor(WHITE);
+                rectangle(x1,y1,x2,y2);
+
+                break;
+            }
+            case '5':
+            {
+                  closegraph();
+            }
+        }
+	}
+	return 0;
+}
